a2badeaed5adf6d503852f5a5e2c001af6f8eaff
Yagiz ALP <yagizwasmeow@gmail.com>
2020-05-25 16:45:08 +0300

    Update server.lua

--- esx_mailbox/server.lua
+++ esx_mailbox/server.lua
@@ -1,4 +1,4 @@
-local ESX = nil
+ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
@@ -8,14 +8,11 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterServerEvent('GS_cassetta:setTitle')
 AddEventHandler('GS_cassetta:setTitle', function(title, dbid)
-
 	MySQL.Async.execute('UPDATE `mail_setting` SET `value` = "' .. title .. '" WHERE `key` = "title" AND dbid = "' .. dbid .. '"', {}, function(rowsChanged) end)
-	
 end)
 
 RegisterServerEvent('GS_cassetta:addMail')
 AddEventHandler('GS_cassetta:addMail', function(title, text, footer, autor, dbid)
-	
 	MySQL.Sync.execute("INSERT INTO `mail` (`id`, `dbid`, `title`, `text`, `footer`, `data`, `autor`, `unread`) VALUES (NULL, @dbid, @title, @text, @footer, @datum, @autor, 1)", {
 		['@title'] = title,
 		['@text'] = text,
@@ -24,28 +21,21 @@ AddEventHandler('GS_cassetta:addMail', function(title, text, footer, autor, dbid
 		['@autor'] = autor,
 		['@dbid'] = dbid}
 	)
-	
 end)
 
 RegisterServerEvent('GS_cassetta:setRead')
 AddEventHandler('GS_cassetta:setRead', function(id)
-	
 	MySQL.Async.execute('UPDATE `mail` SET `unread` = "0" WHERE `id` = "' .. id .. '"', {}, function(rowsChanged) end)
-	
 end)
 
 RegisterServerEvent('GS_cassetta:setUnread')
 AddEventHandler('GS_cassetta:setUnread', function(id)
-	
 	MySQL.Async.execute('UPDATE `mail` SET `unread` = "1" WHERE `id` = "' .. id .. '"', {}, function(rowsChanged) end)
-	
 end)
 
 RegisterServerEvent('GS_cassetta:delete')
 AddEventHandler('GS_cassetta:delete', function(id)
-	
 	MySQL.Async.execute('DELETE FROM `mail` WHERE `mail`.`id` = "' .. id .. '"', {}, function(rowsChanged) end)
-	
 end)
 
 ---------------------------------------------
@@ -77,7 +67,6 @@ ESX.RegisterServerCallback('GS_cassetta:getMail', function(source, cb, dbid)
 end)
 
 ESX.RegisterServerCallback('GS_cassetta:hasItem', function(source, cb, itemname)
-
 	local _source = source
 	local sourceXPlayer = ESX.GetPlayerFromId(_source)
 	local xItem = sourceXPlayer.getInventoryItem(itemname)
@@ -87,5 +76,4 @@ ESX.RegisterServerCallback('GS_cassetta:hasItem', function(source, cb, itemname)
 	else
 	   cb(false)
 	end
-
 end)
